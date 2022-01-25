b19766b3d1315ac1d94b3c3a17156d7b2da6f2c0
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-25 10:59:55 -0600

    index/create routes added

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ db.on('connected', () => console.log('Connected to MongoDB'));
 db.on('disconnected', () => console.log('Disconnected from MongoDB'));
 db.on('error', (err) => console.log('MongoDB Error: ' + err.message));
 // =======================================
-//           SET UP PEOPLE MODEL
+//           SET UP CHEESE MODEL
 // =======================================
 const cheeseSchema = new mongoose.Schema({
     name: String,
@@ -49,6 +49,25 @@ app.use(cors());
 // =======================================
 app.get('/', (req, res) => {
     res.send('welcome to the cheese api');
+});
+// =======================================
+//                 ROUTES
+// =======================================
+// INDEX
+app.get('/cheese', async (req, res) => {
+    try {
+        res.json(await Cheese.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+// CREATE
+app.post('/cheese', async (req, res) => {
+    try {
+        res.json(await Cheese.create(req.body));
+    } catch (error) {
+        res.json(400).json(error);   
+    }
 });
 // =======================================
 //              APP LISTENER
